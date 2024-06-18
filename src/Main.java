e24127a2c9d577e2f82397163d6c06258d745dce
Tim <tcastor1900@gmail.com>
2024-06-18 19:38:46 +0200

    v3

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
-
-
 public class Main {
-    public static void main(String[] args) {
 
-        String vokabel_deutsch = "Hund";
-        String vokabel_englisch = "Dog";
+    public static void main(String[] args) {
+        Vokabel v1 = new Vokabel();
+        v1.vokabel_deutsch="Hund";
+        v1.vokabel_englisch="Dog";
+
+        Vokabel v2 = new Vokabel();
+        v2.vokabel_deutsch="Katze";
+        v2.vokabel_englisch="Cat";
+
         String neue_runde = "nein";
+
 
         Scanner sc = new Scanner(System.in);
 
@@ -15,18 +20,38 @@ public class Main {
         neue_runde = sc.next();
 
         while (neue_runde.equalsIgnoreCase("ja")) {   //Abfrage nach neuer Runde
-            System.out.println(vokabel_deutsch);
-            System.out.println("Gib die Übersetzung ein!");
-            String uebersetzung = sc.next();
-            if (uebersetzung.equalsIgnoreCase(vokabel_englisch)) {  //Vergleich ob Vokabel korrekt
-                System.out.println("Das ist richtig!");
-            } else {
-                System.out.println("Das ist falsch!");
-            }
+
+            trainiereVokabel(v1);
+            trainiereVokabel(v2);
+
             System.out.println("Neue Trainingsrunde? ja/nein");
-            neue_runde=sc.next();
+            neue_runde = sc.next();
         }
 
+        System.out.println("Du hast "+v1.anzahl_trainings +" Runden gespielt");
 
+    }
+    private static void trainiereVokabel(Vokabel v){
+        v.anzahl_trainings++;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(v.vokabel_deutsch);
+        System.out.println("Gib die Übersetzung ein!");
+
+        String uebersetzung = sc.next();
+
+        if (uebersetzung.equalsIgnoreCase(v.vokabel_englisch)) {  //Vergleich ob Vokabel korrekt
+            System.out.println("Das ist richtig!");
+            v.anzahl_erfolgreich++;
+        } else {
+            System.out.println("Das ist falsch!");
+            System.out.println("Richtige Lösung anzeigen ? ja/nein");
+            String loesen = sc.next();
+            if(loesen.equalsIgnoreCase("ja")){
+                System.out.println(v.vokabel_englisch+" <<< richtige Vokabel");
+            }
+
+        }
     }
 }
---
+++ src/Vokabel.java
@@ -0,0 +1,8 @@
+public class Vokabel {
+
+    String vokabel_deutsch ;
+    String vokabel_englisch ;
+
+    int anzahl_trainings = 0;
+    int anzahl_erfolgreich = 0;
+}
