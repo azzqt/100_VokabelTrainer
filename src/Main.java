d73467c5634521cc7dcd4be65d1e4f590d3b1747
Tim Castor <tcastor1900@gmail.com>
2024-06-19 12:42:36 +0200

    v4

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,53 @@
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        Vokabel v1 = new Vokabel();
-        v1.vokabel_deutsch="Hund";
-        v1.vokabel_englisch="Dog";
 
-        Vokabel v2 = new Vokabel();
-        v2.vokabel_deutsch="Katze";
-        v2.vokabel_englisch="Cat";
+    //Vokabel als Objekt erzeuge, mit Standardwerten-Konstruktor deutsch u englisch
+        Vokabel v1 = new Vokabel("Hund","Dog");
+        Vokabel v2 = new Vokabel("Katze", "Cat");
 
         String neue_runde = "nein";
-
-
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Willkommen zum Vokabeltrainer!");
         System.out.println("Neue Trainingsrunde? ja/nein");
         neue_runde = sc.next();
 
-        while (neue_runde.equalsIgnoreCase("ja")) {   //Abfrage nach neuer Runde
-
+//Abfrage nach neuer Runde
+        while (neue_runde.equalsIgnoreCase("ja")) {
+//Methoden Vokabelabfrage aufrufen
             trainiereVokabel(v1);
             trainiereVokabel(v2);
-
             System.out.println("Neue Trainingsrunde? ja/nein");
             neue_runde = sc.next();
         }
 
-        System.out.println("Du hast "+v1.anzahl_trainings +" Runden gespielt");
-
+        System.out.println("Du hast " + v1.getAnzahl_trainings() + " Runden gespielt");
+        System.out.println("Erfolgsquote: "+v1.getErfolgsquote());
     }
-    private static void trainiereVokabel(Vokabel v){
-        v.anzahl_trainings++;
+
+//METHODE Vokabel abfragen
+    private static void trainiereVokabel(Vokabel v) {
+        v.update_anzahlTrainings();
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println(v.vokabel_deutsch);
+        System.out.println(v.getVokabel_deutsch());
         System.out.println("Gib die Übersetzung ein!");
 
         String uebersetzung = sc.next();
-
-        if (uebersetzung.equalsIgnoreCase(v.vokabel_englisch)) {  //Vergleich ob Vokabel korrekt
+//Vergleich ob Vokabel korrekt
+        if (uebersetzung.equalsIgnoreCase(v.getVokabel_englisch())) {
             System.out.println("Das ist richtig!");
-            v.anzahl_erfolgreich++;
+            v.update_anzahlErfolgreich();
         } else {
             System.out.println("Das ist falsch!");
             System.out.println("Richtige Lösung anzeigen ? ja/nein");
             String loesen = sc.next();
-            if(loesen.equalsIgnoreCase("ja")){
-                System.out.println(v.vokabel_englisch+" <<< richtige Vokabel");
+            if (loesen.equalsIgnoreCase("ja")) {
+                System.out.println(v.getVokabel_englisch() + " <<< richtige Vokabel");
             }
 
         }
--- src/Vokabel.java
+++ src/Vokabel.java
@@ -1,8 +1,40 @@
 public class Vokabel {
 
-    String vokabel_deutsch ;
-    String vokabel_englisch ;
+    public Vokabel(String vokabel_deutsch, String vokabel_englisch) { //Konstruktor der Standardwerte vorgibt die eingegeben werden muessen
 
-    int anzahl_trainings = 0;
-    int anzahl_erfolgreich = 0;
+        this.vokabel_deutsch = vokabel_deutsch;
+        this.vokabel_englisch = vokabel_englisch;
+    }
+//public Zugriffs get methoden, damit die Vokabel lesbar wird
+    public String getVokabel_deutsch(){
+        return this.vokabel_deutsch;
+    }
+    public String getVokabel_englisch(){
+        return this.vokabel_englisch;
+    }
+//Private String damit man sie nicht ausversehen aendern kann
+    private String vokabel_deutsch;
+    private String vokabel_englisch;
+
+//Methode zum erhöhen der erfolgreichen und trainings
+    public void update_anzahlTrainings(){
+        anzahl_trainings++;
+    }
+    public void update_anzahlErfolgreich(){
+        anzahl_erfolgreich++;
+    }
+
+//Private int damit man sie nicht ausversehen aendern kann
+    private int anzahl_trainings = 0;
+    private int anzahl_erfolgreich = 0;
+
+//Methoden zum Aufruf der absolvierten Trainings
+    public int getAnzahl_trainings(){
+        return anzahl_trainings;
+    }
+//Erfolgsquoute berechnen in duble wegen komma
+    public double getErfolgsquote(){
+        return (double) this.anzahl_erfolgreich/this.anzahl_trainings;
+}
+
 }
