5b547b11a17a671c5c91a1a047dda1813d4f9f5c
Tim Castor <tcastor1900@gmail.com>
2024-06-19 13:34:35 +0200

    v5

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,61 @@ public class Main {
 
     public static void main(String[] args) {
 
-    //Vokabel als Objekt erzeuge, mit Standardwerten-Konstruktor deutsch u englisch
-        Vokabel v1 = new Vokabel("Hund","Dog");
+//Vokabel als Objekt erzeuge, mit Standardwerten-Konstruktor deutsch u englisch
+        Vokabel v1 = new Vokabel("Hund", "Dog");
         Vokabel v2 = new Vokabel("Katze", "Cat");
 
-        String neue_runde = "nein";
+        Vokabel[] vokabelnArray = new Vokabel[10]; //Array vokabeldatentyp erzeugen
+        vokabelnArray[0] = v1;
+        vokabelnArray[1] = v2;
+        int index = 2;// speichert, die menge an vokabeln im array
+
+
+        String eingabe;
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Willkommen zum Vokabeltrainer!");
-        System.out.println("Neue Trainingsrunde? ja/nein");
-        neue_runde = sc.next();
+        System.out.println("Neue Vokabel (n), neues Training (t), Beenden (x)");
+        eingabe = sc.next();
 
-//Abfrage nach neuer Runde
-        while (neue_runde.equalsIgnoreCase("ja")) {
-//Methoden Vokabelabfrage aufrufen
-            trainiereVokabel(v1);
-            trainiereVokabel(v2);
-            System.out.println("Neue Trainingsrunde? ja/nein");
-            neue_runde = sc.next();
+//Spielstart, Abfrage nach neuer Runde
+        while (!eingabe.equalsIgnoreCase("x")) { //spiel laeuft solange man nicht X drueckt
+            if (eingabe.equalsIgnoreCase("t")) {  //training starten
+
+                //Methoden Vokabelabfrage aufrufen
+                for (int i = 0; i <= index - 1; i++) {
+
+                    Vokabel v = vokabelnArray[i];
+                    trainiereVokabel(v);
+
+                }
+
+
+            }
+            if (eingabe.equalsIgnoreCase("n")) {//eigene neue vokabel anlegen
+              if(index< vokabelnArray.length){
+                System.out.println("Vokabel deutsch:");
+                String vd = sc.next();
+                System.out.println("Vokabel englisch:");
+                String ve = sc.next();
+                Vokabel v = new Vokabel(vd, ve);         //neue vokabel erschaffen mit konstruktor standardwert deutsch und englisch
+                vokabelnArray[index] = v;
+                index++;}
+              else{
+                  System.out.println("Kein freier Speicherplatz");
+              }
+            }
+
+
+            System.out.println("Neue Vokabel (n), neues Training (t), Beenden (x)");
+            eingabe = sc.next();
         }
 
         System.out.println("Du hast " + v1.getAnzahl_trainings() + " Runden gespielt");
-        System.out.println("Erfolgsquote: "+v1.getErfolgsquote());
+        System.out.println("Erfolgsquote: " + v1.getErfolgsquote());
     }
 
-//METHODE Vokabel abfragen
+    //METHODE Vokabel abfragen
     private static void trainiereVokabel(Vokabel v) {
         v.update_anzahlTrainings();
 
--- src/Vokabel.java
+++ src/Vokabel.java
@@ -1,40 +1,41 @@
 public class Vokabel {
+    //Private String damit man sie nicht ausversehen aendern kann
+    private String vokabel_deutsch;
+    private String vokabel_englisch;
 
-    public Vokabel(String vokabel_deutsch, String vokabel_englisch) { //Konstruktor der Standardwerte vorgibt die eingegeben werden muessen
+    //Konstruktor der Standardwerte vorgibt die eingegeben werden muessen
+    public Vokabel(String vokabel_deutsch, String vokabel_englisch) {
 
         this.vokabel_deutsch = vokabel_deutsch;
         this.vokabel_englisch = vokabel_englisch;
     }
-//public Zugriffs get methoden, damit die Vokabel lesbar wird
-    public String getVokabel_deutsch(){
+
+    //public Zugriffs get methoden, damit die Vokabel lesbar wird
+    public String getVokabel_deutsch() {
         return this.vokabel_deutsch;
     }
-    public String getVokabel_englisch(){
+
+    public String getVokabel_englisch() {
         return this.vokabel_englisch;
     }
-//Private String damit man sie nicht ausversehen aendern kann
-    private String vokabel_deutsch;
-    private String vokabel_englisch;
 
-//Methode zum erhöhen der erfolgreichen und trainings
-    public void update_anzahlTrainings(){
-        anzahl_trainings++;
-    }
-    public void update_anzahlErfolgreich(){
-        anzahl_erfolgreich++;
-    }
-
-//Private int damit man sie nicht ausversehen aendern kann
+    //Private int damit man sie nicht ausversehen aendern kann
     private int anzahl_trainings = 0;
     private int anzahl_erfolgreich = 0;
-
-//Methoden zum Aufruf der absolvierten Trainings
-    public int getAnzahl_trainings(){
+    //Methode zum erhöhen der erfolgreichen und trainings
+    public void update_anzahlTrainings() {
+        anzahl_trainings++;
+    }
+    public void update_anzahlErfolgreich() {
+        anzahl_erfolgreich++;
+    }
+    //Methoden zum Aufruf der absolvierten Trainings
+    public int getAnzahl_trainings() {
         return anzahl_trainings;
     }
-//Erfolgsquoute berechnen in duble wegen komma
-    public double getErfolgsquote(){
-        return (double) this.anzahl_erfolgreich/this.anzahl_trainings;
-}
+    //Erfolgsquoute berechnen in duble wegen komma
+    public double getErfolgsquote() {
+        return (double) this.anzahl_erfolgreich / this.anzahl_trainings;
+    }
 
 }
